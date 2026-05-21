3787bae5e064e942328eaa85c1a820e753b59373
deviank <70948471+DevianKeno@users.noreply.github.com>
2026-05-21 22:17:52 +0800

    feat (Logger): update entire lib
    untested 🥀 straight to prod

--- src/Logger.ts
+++ src/Logger.ts
@@ -13,13 +13,52 @@ export enum LogLevel {
 	NONE = 4,
 }
 
+const LogLevelNames: string[] = ["DEBUG", "INFO", "WARN", "ERROR", "NONE"];
+
 export class Logger {
 	private static _globalLevel: LogLevel = LogLevel.DEBUG;
 	private static readonly loggers: Map<string, Logger> = new Map();
-	private constructor(private name: string) {}
 
 	static get globalLevel(): LogLevel {
 		return this._globalLevel;
+	}
+
+	private _debug: Logger;
+	private _info: Logger;
+	private _warn: Logger;
+	private _error: Logger;
+
+	get atDebug(): Logger {
+		return this._debug;
+	}
+
+	get atInfo(): Logger {
+		return this._info;
+	}
+
+	get atWarn(): Logger {
+		return this._warn;
+	}
+
+	get atError(): Logger {
+		return this._error;
+	}
+
+	private _logLevel: LogLevel;
+
+	get logLevel(): LogLevel {
+		return this._logLevel;
+	}
+
+	private constructor(
+		private name: string,
+		logLevel?: LogLevel
+	) {
+		this._logLevel = logLevel ?? Logger._globalLevel;
+		this._debug = new Logger(`${this.name}.debug`, LogLevel.DEBUG);
+		this._info = new Logger(`${this.name}.info`, LogLevel.INFO);
+		this._warn = new Logger(`${this.name}.warn`, LogLevel.WARN);
+		this._error = new Logger(`${this.name}.error`, LogLevel.ERROR);
 	}
 
 	static setGlobalLevel(level: LogLevel) {
@@ -30,30 +69,40 @@ export class Logger {
 		return MapUtils.computeIfUndefined(this.loggers, name, () => new Logger(name));
 	}
 
-	debug(message: any, ...params: any[]) {
-		this.log(LogLevel.DEBUG, "DEBUG", console.warn, message, ...params);
+	at(level: LogLevel) {
+		return {
+			0: this._debug,
+			1: this._info,
+			2: this._warn,
+			3: this._error,
+			4: this,
+		}[level];
+	}
+
+	log(message: any, ...params: any[]) {
+		this._log(console.log, message, ...params);
 	}
 
 	info(message: any, ...params: any[]) {
-		this.log(LogLevel.INFO, "INFO", console.warn, message, ...params);
+		this._log(console.info, message, ...params);
 	}
 
 	warn(message: any, ...params: any[]) {
-		this.log(LogLevel.WARN, "WARN", console.warn, message, ...params);
+		this._log(console.warn, message, ...params);
 	}
 
 	error(message: any, ...params: any[]) {
-		this.log(LogLevel.ERROR, "ERROR", console.error, message, ...params);
+		this._log(console.error, message, ...params);
 	}
 
+	//#region Log methods
+
 	toWorld(message: any, ...params: any[]) {
-		this.log(LogLevel.DEBUG, "LOGGER", world.sendMessage, message, ...params);
+		this._log(world.sendMessage, message, ...params);
 	}
 
 	toPlayer(player: Player, message: any, ...params: any[]) {
-		this.log(
-			LogLevel.DEBUG,
-			"LOGGER",
+		this._log(
 			(message?: any, ...optionalParams: any[]) => {
 				player.sendMessage(message);
 			},
@@ -62,10 +111,14 @@ export class Logger {
 		);
 	}
 
+	/**
+	 * Logs a message to the player's actionbar.
+	 * @param player
+	 * @param message
+	 * @param params
+	 */
 	actionbar(player: Player, message: any, ...params: any[]) {
-		this.log(
-			LogLevel.DEBUG,
-			"INFO",
+		this._log(
 			(message?: any, ...optionalParams: any[]) => {
 				TitleUtils.actionbar(player, message);
 			},
@@ -76,14 +129,12 @@ export class Logger {
 
 	/**
 	 * Logs a message on behalf of an entity using the `/say` command.
-	 * @param entity 
-	 * @param message 
-	 * @param params 
+	 * @param entity
+	 * @param message
+	 * @param params
 	 */
 	asEntity(entity: Entity, message: any, ...params: any[]) {
-		this.log(
-			LogLevel.DEBUG,
-			"LOGGER",
+		this._log(
 			(message?: any, ...optionalParams: any[]) => {
 				entity.runCommand(`say ${message}`);
 			},
@@ -92,17 +143,19 @@ export class Logger {
 		);
 	}
 
-	private shouldLog(level: LogLevel): boolean {
+	//#endregion
+
+	private _shouldLog(level: LogLevel): boolean {
 		return level >= Logger._globalLevel;
 	}
 
-	private format(level: string, message: any): string {
+	private _format(level: string, message: any): string {
 		return `[${level}] [${this.name}] ${message}`;
 	}
 
-	private log(level: LogLevel, levelName: string, method: LogMethod, message: any, ...params: any[]) {
-		if (!this.shouldLog(level)) return;
+	private _log(method: LogMethod, message: any, ...params: any[]) {
+		if (!this._shouldLog(this._logLevel)) return;
 
-		method(this.format(levelName, message), ...params);
+		method(this._format(LogLevelNames[this._logLevel], message), ...params);
 	}
 }
