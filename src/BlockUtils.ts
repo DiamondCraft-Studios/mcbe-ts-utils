f5db3e0ecd67147dc77a8f423734e94c9fb427b3
deviank <70948471+DevianKeno@users.noreply.github.com>
2026-05-22 18:29:45 +0800

    feat (BlockUtils): update functions

--- src/BlockUtils.ts
+++ src/BlockUtils.ts
@@ -4,7 +4,7 @@ import { Block, Direction, Vector3 } from "@minecraft/server";
  * Block utility functions.
  */
 export class BlockUtils {
-	static readonly directionToOffset = new Map<Direction, Vector3>([
+	static readonly directionToVector = new Map<Direction, Vector3>([
 		[Direction.North, { x: 0, y: 0, z: -1 }],
 		[Direction.South, { x: 0, y: 0, z: 1 }],
 		[Direction.East, { x: 1, y: 0, z: 0 }],
@@ -15,20 +15,41 @@ export class BlockUtils {
 
 	/**
 	 * Gets the vector offset of the given direction from the block.
-	 * @param direction 
-	 * @returns 
+	 * @obsolete Use `getDirectionVector` instead.
+	 * @param direction
+	 * @returns
 	 */
 	static getDirectionOffset(direction: Direction): Vector3 {
-		return this.directionToOffset.get(direction) ?? { x: 0, y: 0, z: 0 };
+		return this.getDirectionVector(direction);
 	}
-	
+
+	/**
+	 * Gets the vector offset of the given direction from the block.
+	 * @param direction
+	 * @returns
+	 */
+	static getDirectionVector(direction: Direction): Vector3 {
+		return this.directionToVector.get(direction) ?? { x: 0, y: 0, z: 0 };
+	}
+
 	/**
 	 * Gets the location of the given block relative to the given direction.
-	 * @param block 
-	 * @param direction 
-	 * @returns 
+	 * @obsolete Use `getLocationOnFace` instead.
+	 * @param block
+	 * @param direction
+	 * @returns
 	 */
 	static getRelativeLocation(block: Block, direction: Direction): Vector3 {
+		return this.getLocationOnFace(block, direction);
+	}
+
+	/**
+	 * Gets the location of the given block relative to the given direction.
+	 * @param block
+	 * @param direction
+	 * @returns
+	 */
+	static getLocationOnFace(block: Block, direction: Direction): Vector3 {
 		const offset = this.getDirectionOffset(direction);
 		return {
 			x: block.location.x + offset.x,
@@ -38,18 +59,20 @@ export class BlockUtils {
 	}
 
 	/**
- 	 * Gets the block's bottom-center position.
-	 * @param block 
-	 * @returns 
+	 * Gets the block beside this block given a direction.
+	 * @param block
+	 * @param direction
+	 * @returns
 	 */
-	static getBottomCenter(block: Block): Vector3 {
-		return { ...block.center(), y: block.location.y };
+	static getBlockOnFace(block: Block, direction: Direction): Block | undefined {
+		const location = this.getRelativeLocation(block, direction);
+		return block.dimension.getBlock(location);
 	}
 
 	/**
 	 * Gets the block beside this block given it's cardinal direction property.
-	 * @param block 
-	 * @returns 
+	 * @param block
+	 * @returns
 	 */
 	static getBlockOnCardinal(block: Block): Block | undefined {
 		const cardinal = block.permutation.getState("minecraft:cardinal_direction");
@@ -67,13 +90,34 @@ export class BlockUtils {
 	}
 
 	/**
+	 * Gets the block's bottom-center position.
+	 * @param block
+	 * @returns
+	 */
+	static getBottomCenter(block: Block): Vector3 {
+		return { ...block.center(), y: block.location.y };
+	}
+
+	/**
 	 * Checks if the given block is within the given coordinate bounds.
-	 * @param minInclusive 
-	 * @param maxInclusive 
-	 * @param block 
-	 * @returns 
+	 * @obsolete Use `isWithinAreaBox` instead.
+	 * @param minInclusive
+	 * @param maxInclusive
+	 * @param block
+	 * @returns
 	 */
 	static isWithinBounds(minInclusive: Vector3, maxInclusive: Vector3, block: Block): boolean {
+		return this.isWithinAreaBox(block, minInclusive, maxInclusive);
+	}
+
+	/**
+	 * Checks if the given block is within the given coordinate bounds.
+	 * @param minInclusive
+	 * @param maxInclusive
+	 * @param block
+	 * @returns
+	 */
+	static isWithinAreaBox(block: Block, minInclusive: Vector3, maxInclusive: Vector3): boolean {
 		const { x, y, z } = block.location;
 
 		const minX = Math.min(minInclusive.x, maxInclusive.x);
