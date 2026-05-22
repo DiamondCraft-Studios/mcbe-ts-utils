13f4e6a8c9df9b8e40b89fb87d6c48605123f6d8
deviank <70948471+DevianKeno@users.noreply.github.com>
2026-05-22 14:38:28 +0800

    feat (TitleUtils): add title functions
    untested 🥀 straight to prod

--- src/TitleUtils.ts
+++ src/TitleUtils.ts
@@ -5,9 +5,37 @@ import { Player, RawMessage } from "@minecraft/server";
  */
 export class TitleUtils {
 	/**
+	 * Sends a title message to the player.
+	 * @param player Target player.
+	 * @param title The title text.
+	 * @param subtitle Optional subtitle.
+	 */
+	static title(player: Player, title: string, subtitle?: string) {
+		if (subtitle) {
+			player.runCommand(`title @s title ${title} subtitle ${subtitle}`);
+		} else {
+			player.runCommand(`title @s title ${title}`);
+		}
+	}
+
+	/**
+	 * Sends a raw title message to the player.
+	 * @param player Target player.
+	 * @param title The title text.
+	 * @param subtitle Optional subtitle.
+	 */
+	static titleRaw(player: Player, title: RawMessage, subtitle?: RawMessage) {
+		if (subtitle) {
+			player.runCommand(`titleraw @s title ${JSON.stringify(title)} subtitle ${JSON.stringify(subtitle)}`);
+		} else {
+			player.runCommand(`titleraw @s title ${JSON.stringify(title)}`);
+		}
+	}
+
+	/**
 	 * Sends an actionbar message to the player.
-	 * @param player 
-	 * @param message 
+	 * @param player
+	 * @param message
 	 */
 	static actionbar(player: Player, message: string) {
 		player.runCommand(`title @s actionbar ${message}`);
@@ -15,8 +43,8 @@ export class TitleUtils {
 
 	/**
 	 * Sends a raw actionbar message to the player.
-	 * @param player 
-	 * @param message 
+	 * @param player
+	 * @param message
 	 */
 	static actionbarRaw(player: Player, message: RawMessage) {
 		player.runCommand(`titleraw @s actionbar ${JSON.stringify(message)}`);
