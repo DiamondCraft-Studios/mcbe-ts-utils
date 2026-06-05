1ef61a18d4895c0e9a9926d4eaceca333a52307c
deviank <70948471+DevianKeno@users.noreply.github.com>
2026-06-05 16:29:57 +0800

    feat (DimensionUtils): update fns

--- src/DimensionUtils.ts
+++ src/DimensionUtils.ts
@@ -6,39 +6,53 @@ import { Dimension, EntityQueryOptions, world } from "@minecraft/server";
 export class DimensionUtils {
 	/**
 	 * Gets the overworld dimension.
-	 * @returns 
+	 * @returns
 	 */
 	static getOverworld() {
 		return world.getDimension("overworld");
 	}
+
 	/**
 	 * Gets the Nether dimension.
-	 * @returns 
+	 * @returns
 	 */
 	static getNether() {
 		return world.getDimension("nether");
 	}
+
 	/**
 	 * Gets the End dimension.
-	 * @returns 
+	 * @returns
 	 */
 	static getEnd() {
 		return world.getDimension("the_end");
 	}
+
 	/**
 	 * Removes all entities in the given dimension matching the given query options.
-	 * @param dimension 
-	 * @param options 
+	 * @param dimension
+	 * @param options
 	 */
 	static removeEntities(dimension: Dimension, options: EntityQueryOptions) {
 		dimension.getEntities(options).forEach((entity) => entity?.remove());
 	}
+
 	/**
 	 * Runs the given commands in the given dimension.
-	 * @param dimension 
-	 * @param commands 
+	 * @obsolete Use `runCommandsIn` instead.
+	 * @param dimension
+	 * @param commands
 	 */
 	static runCommands(dimension: Dimension, commands: string[]) {
+		this.runCommandsIn(dimension, ...commands);
+	}
+
+	/**
+	 * Runs the given commands in the given dimension.
+	 * @param dimension
+	 * @param commands
+	 */
+	static runCommandsIn(dimension: Dimension, ...commands: string[]) {
 		commands.forEach((command) => {
 			try {
 				dimension.runCommand(command);
